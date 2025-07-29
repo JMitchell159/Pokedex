dc036fec979a67e8061664edc8b0ec2052855c45
JMichell159 <joemitchell159@gmail.com>
2025-07-29 07:19:57 -0500

    removed pokedex binary file since it should not be tracked, main.go is now a repl

--- main.go
+++ main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"bufio"
+	"os"
 )
 
 func cleanInput(text string) []string {
-	var result []string;
+	trimmed := strings.TrimSpace(text)
+	split := strings.Split(trimmed, " ")
+	temp := make([]string, len(split))
+	idx := -1
+	for _, word := range split {
+		if len(word) > 0 {
+			idx++
+			temp[idx] = strings.ToLower(word)
+		}
+	}
+	result := temp[:idx + 1]
 	return result;
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	sc := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		sc.Scan()
+		clean := cleanInput(sc.Text())
+		fmt.Printf("Your command was: %s\n", clean[0])
+	}
 }
