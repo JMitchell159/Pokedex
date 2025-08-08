78fe156f3ba5dffffcb67012596e1f5285e2cc5e
JMichell159 <joemitchell159@gmail.com>
2025-08-08 07:17:07 -0500

    added map and mapb commands

--- cli_commands.go
+++ cli_commands.go
@@ -3,12 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"net/http"
+	"encoding/json"
+	"io"
 )
+
+type result struct {
+	name	string	`json:"name"`
+	url		string	`json:"url"`
+}
+
+type config struct {
+	next		string		`json:"next"`
+	previous	string		`json:"previous"`
+	results		[]result	`json:"results"`
+}
 
 type cliCommand struct {
 	name		string
 	description	string
 	callback	func() error
+	configPtr	*config
 }
 
 func commandExit() error {
@@ -17,11 +32,82 @@ func commandExit() error {
 	return nil
 }
 
-func help(commandMap map[string]cliCommand) func() error {
+func help(cliMap map[string]cliCommand) func() error {
 	return func() error {
 		fmt.Print("Welcome to the Pokedex!\nUsage\n\n")
-		for _, val := range commandMap {
+		for _, val := range cliMap {
 			fmt.Printf("%s: %s\n", val.name, val.description)
+		}
+		return nil
+	}
+}
+
+func commandMap(configPtr *config) func() error {
+	return func() error {
+		req, err := http.NewRequest("GET", configPtr.next, nil)
+		if err != nil {
+			return fmt.Errorf("Couldn't make the request struct: %v\n", err)
+		}
+
+		client := &http.Client{}
+
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/json")
+		res, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("Something wrong with the response: %v\n", err)
+		}
+
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+
+		if res.StatusCode > 299 {
+			return fmt.Errorf("Response failed with status code: %d and \nbody: %s\n", res.StatusCode, body)
+		}
+
+		if err != nil {
+			return fmt.Errorf("Error reading response body: %v\n", err)
+		}
+
+		fmt.Printf("Response Body (string): %s\n", string(body))
+
+		err = json.Unmarshal(body, configPtr)
+		if err != nil {
+			return err
+		}
+		for _, result := range configPtr.results {
+			fmt.Printf("%s\n", result.name)
+		}
+		return nil
+	}
+}
+
+func mapb(configPtr *config) func() error {
+	return func() error {
+		if configPtr.previous == "null" {
+			fmt.Print("you're on the first page\n")
+		} else {
+			res, err := http.Get(configPtr.previous)
+			if err != nil {
+				return err
+			}
+			body, err := io.ReadAll(res.Body)
+			res.Body.Close()
+			if res.StatusCode > 299 {
+				return fmt.Errorf("Response failed with status code: %d and \nbody: %s\n", res.StatusCode, body)
+			}
+			if err != nil {
+				return err
+			}
+			dat := []byte(body)
+			err = json.Unmarshal(dat, configPtr)
+			if err != nil {
+				return err
+			}
+			for _, result := range configPtr.results {
+				fmt.Printf("%s\n", result.name)
+			}
 		}
 		return nil
 	}
--- main.go
+++ main.go
@@ -24,17 +24,37 @@ func cleanInput(text string) []string {
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
+	r := make([]result, 20)
+	cfg := config{
+		next:		"https://pokeapi.co/api/v2/location-area",
+		previous:	"null",
+		results:	r,
+	}
 	cliMap := map[string]cliCommand{
 		"exit": {
 			name:			"exit",
 			description:	"Exit the Pokedex",
 			callback:		commandExit,
+			configPtr:		&cfg,
+		},
+		"map": {
+			name:			"map",
+			description:	"Lists the next 20 location areas",
+			callback:		commandMap(&cfg),
+			configPtr:		&cfg,
+		},
+		"mapb": {
+			name:			"mapb",
+			description:	"Lists the previous 20 location areas",
+			callback:		mapb(&cfg),
+			configPtr:		&cfg,
 		},
 	}
 	cliMap["help"] = cliCommand{
 		name:			"help",
 		description:	"Displays a help message",
 		callback:		help(cliMap),
+		configPtr:		&cfg,
 	}
 
 	for {
@@ -46,7 +66,7 @@ func main() {
 			if clean[0] == command {
 				err := cliMap[command].callback()
 				if err != nil {
-					fmt.Errorf("%v", err)
+					fmt.Printf("%v", err)
 				}
 				valid = true
 				break
